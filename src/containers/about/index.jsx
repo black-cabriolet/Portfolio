460e8e963587f01aa0d5972efe380e438af8cab2
black-cabriolet <kinyanjuityreek5@gmail.com>
2025-01-20 09:45:53 +0300

    v5

--- src/containers/about/index.jsx
+++ src/containers/about/index.jsx
@@ -30,7 +30,8 @@ const personalDetails = [
 ];
 
 const jobSummary =
-    "Seasoned and independent Front End Developer with 5 years of experience in blending the art of design with skill of programming to deliver an immersive and engaging user experience through efficient website development, proactive feature optimization, and relentless debugging. Very passionate about aesthetics and UI design. It is imperative that you provide a thorough and professional approach to your resume. As a Front End Developer you will be judged by your ability to use UX and UI concepts and follow design guidelines. It is about expressing your attention to detail and how you can help implement design ideas for your future employer";
+    "Seasoned and independent Front End Developer with 2 years of experience in blending the art of design with" +
+    " skill of programming to deliver an immersive and engaging user experience through efficient website development, proactive feature optimization, and relentless debugging. Very passionate about aesthetics and UI design. It is imperative that you provide a thorough and professional approach to your resume. As a Front End Developer you will be judged by your ability to use UX and UI concepts and follow design guidelines. It is about expressing your attention to detail and how you can help implement design ideas for your future employer";
 
 
 const About=()=>{
